bcf35f0274ca69056e33bfabe225bb6352f5ca25
Angélique <angeliquelebas2008@gmail.com>
2022-03-06 16:52:36 +0100

    fetch no cors

--- src/services/api/pimsAPI.js
+++ src/services/api/pimsAPI.js
@@ -3,38 +3,23 @@ myHeaders.append(
   "Authorization",
   "Basic " + btoa("sandbox:c5jI1ABi8d0x87oWfVzvXALqkf0hToGq")
 );
-console.log("1 : " + myHeaders.get("Authorization"));
-/*
+
 const myInit = {
   method: "GET",
   headers: myHeaders,
-  mode: "no-cors",
-  cache: "default",
 };
-console.log("2 : " + myInit.headers.get("Authorization"));
 
-const myRequest = new Request("https://sandbox.pims.io/api/v1/events");*/
+const myRequest = new Request("https://sandbox.pims.io/api/v1/events");
 
 const getPimsData = async function () {
-  //const response = await fetch(myRequest, myInit);
-  const response = await fetch("https://sandbox.pims.io/api/v1/events", {
-    method: "GET", // or GET, PUT, DELETE, etc.
-    headers: {
-      "Content-Type": "application/json", // to have JSON content in body
-      "Authorization":
-        "Basic " + btoa("sandbox:c5jI1ABi8d0x87oWfVzvXALqkf0hToGq"), // set JWT token
-    },
-    mode: "no-cors",
-  });
+  const response = await fetch(myRequest, myInit);
   if (response.status == 200) {
-    //console.log(response.json());
     return response.json();
   } else {
-    console.log(response.status);
     new Error(response.statusText);
   }
 };
 
 console.log(getPimsData());
 
-export { getPimsData };
+export default { getPimsData };
